63f19df8f7ec803c385d349ebd6442000471c957
Snigdha Sahara <snigdhasahara@Snigdhas-MBP.fios-router.home>
2020-03-11 09:26:20 -0400

    jsx concept html css  in js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var person = {
+    name:"Dr. Mahfuz", 
+    job:"Singer"
+  }
+
+  var person2 = {
+    name:"Eva Rahman", 
+    job:"kokil konthi"
+  }
+
+  var style = {
+    color:'red',
+    backgroundColor:"yellow"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h1 style={style}>My Heading: {person.name +"  " +
+        person.job}</h1>
+
+    <h2 style={{color:'red',backgroundColor:"yellow"}}>My Heading 2: {person2.name + " "+person2.job}</h2>
+        <p>My First React App!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
